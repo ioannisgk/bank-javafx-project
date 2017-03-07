ae3dcad51849aa4d8706bbd82f89f6462fcaa8b2
Ioannis Gkourtzounis <ioannisgk@live.com>
2017-03-08 01:15:49 +0200

    Basic functionality - Starting GUI (constructing main)

--- src/com/company/View.java
+++ src/com/company/View.java
@@ -1,11 +1,18 @@
 package com.company;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+
 import static javafx.application.Application.launch;
 
 public class View extends Application {
@@ -20,41 +27,79 @@ public class View extends Application {
     public static void run (String[] args) {
         launch(args);
     }
-
-    public void start (Stage mainStage) {
+    @Override
+    public void start (Stage mainStage) throws Exception {
 
         window = mainStage;
+        window.setTitle("Bank Application");
 
-        // Label and button for sceneLogin
-        Label labelLogin = new Label("Please enter your login information below");
-        Button buttonLogin = new Button("Login");
+        // GridPane layout with 10px padding
+        GridPane gridLogin = new GridPane();
+        gridLogin.setPadding(new Insets(10, 10, 10, 10));
+        gridLogin.setVgap(8);
+        gridLogin.setHgap(10);
+
+        // Label and text field for username
+        Label labelUsername = new Label("Username:");
+        GridPane.setConstraints(labelUsername, 0, 0);
+        TextField username = new TextField();
+        username.setPromptText("username");
+        GridPane.setConstraints(username, 1, 0);
+
+        // Label and text field for password
+        Label labelPassword = new Label("Password:");
+        GridPane.setConstraints(labelPassword, 0, 1);
+        TextField password = new TextField();
+        password.setPromptText("password");
+        GridPane.setConstraints(password, 1, 1);
+
+        // Button for login
+        Button buttonLogin = new Button("Log In");
+        GridPane.setConstraints(buttonLogin, 1, 2);
         buttonLogin.setOnAction(e -> window.setScene(sceneMain));
 
-        // Set layout with label and button to create scene
-        VBox layoutLogin = new VBox(20);
-        layoutLogin.getChildren().addAll(labelLogin, buttonLogin);
-        sceneLogin = new Scene(layoutLogin, 600, 300);
+        // Add elements to layout and create "sceneLogin"
+        gridLogin.getChildren().addAll(labelUsername, username, labelPassword, password, buttonLogin);
+        sceneLogin = new Scene(gridLogin, 300, 200);
 
+        //////////////////////////////////////////////////////
+
+        // http://tutorials.jenkov.com/javafx/imageview.html
+        FileInputStream imageFile = new FileInputStream("bank_logo.png");
+        Image image = new Image(imageFile);
+        ImageView iv = new ImageView(image);
+        iv.setFitWidth(500);
+        iv.setFitHeight(144);
+        HBox hbox = new HBox(iv);
+
+
+
+        sceneMain = new Scene(hbox, 600, 300);
+
+
+
+        ////
 
         // Label and button for sceneLogin
-        Label labelMain = new Label("Please select an action");
-        Button buttonLogout = new Button("Logout");
-        buttonLogout.setOnAction(e -> logout());
+        //Label labelMain = new Label("Please select an action");
+        //Button buttonLogout = new Button("Logout");
+        //buttonLogout.setOnAction(e -> logout());
 
         // Set layout with label and button to create scene
-        StackPane layoutMain = new StackPane();
-        layoutMain.getChildren().add(buttonLogout);
-        sceneMain = new Scene(layoutMain, 600, 300);
+        //StackPane layoutMain = new StackPane();
+        //layoutMain.getChildren().add(buttonLogout);
+        //sceneMain = new Scene(layoutMain, 600, 300);
+
+        ///////////////////////////////////////////////////////
 
         // Display "sceneLogin" when starting the application
+
         window.setScene(sceneLogin);
-        window.setTitle("Bank Application");
         window.setOnCloseRequest(e -> {
             // Prevent default close action with consume()
             e.consume();
             closeApplication();
         });
-
         window.show();
     }
 
@@ -67,6 +112,11 @@ public class View extends Application {
     private void logout() {
         boolean answer;
         answer = ConfirmBox.display("Confirmation", "Are you sure you want to logout?");
-        if (answer) { window.setScene(sceneLogin); }
+        if (answer) {
+            window.setScene(sceneLogin);
+            // Refresh windows size
+            window.setResizable(!window.isResizable());
+            window.setResizable(window.isResizable());
+        }
     }
 }
