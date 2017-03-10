47e9603e9c39f6617362d30097c4f4496ba83ec2
Ioannis Gkourtzounis <ioannisgk@live.com>
2017-03-10 22:26:20 +0200

    Basic functionality - Starting GUI (created NewAccountView)

--- src/com/company/NewAccountView.java
+++ src/com/company/NewAccountView.java
@@ -2,9 +2,7 @@ package com.company;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -14,7 +12,7 @@ import java.io.FileInputStream;
 public class NewAccountView extends Application {
 
     Stage window;
-    Scene sceneEnterInfo, sceneMain;
+    Scene sceneNewAccount;
 
     public NewAccountView () {
 
@@ -27,110 +25,117 @@ public class NewAccountView extends Application {
     public void start (Stage mainStage) throws Exception {
 
         window = mainStage;
-        window.setTitle("Bank Application222");
+        window.setTitle("Bank Application");
 
-        ///////////////////////////////////////
-        //////// CREATE SCENEENTERINFO ////////
-        ///////////////////////////////////////
+        ////////////////////////////////////////
+        //////// CREATE SCENENEWACCOUNT ////////
+        ////////////////////////////////////////
 
         // GridPane layout with 10px padding
         GridPane gridOpenAccount = new GridPane();
-        gridOpenAccount.setPadding(new Insets(10, 20, 0, 30));
-        gridOpenAccount.setVgap(20);
-        gridOpenAccount.setHgap(0);
-        //gridOpenAccount.setMinHeight(200);
+        gridOpenAccount.setPadding(new Insets(10, 20, 10, 30));
+        gridOpenAccount.setVgap(8);
+        gridOpenAccount.setHgap(10);
+
+        Label labelTitle = new Label("CUSTOMER DATA");
+        GridPane.setConstraints(labelTitle, 0, 0);
 
         // Label and text field for firstname
         Label labelFirstname = new Label("Firstname:");
-        GridPane.setConstraints(labelFirstname, 0, 0);
+        GridPane.setConstraints(labelFirstname, 0, 1);
         TextField firstname = new TextField();
         firstname.setPromptText("firstname");
-        GridPane.setConstraints(firstname, 1, 0);
+        GridPane.setConstraints(firstname, 1, 1);
 
         // Label and text field for surname
         Label labelSurname = new Label("Surname:");
-        GridPane.setConstraints(labelSurname, 0, 1);
+        GridPane.setConstraints(labelSurname, 0, 2);
         TextField surname = new TextField();
         surname.setPromptText("surname");
-        GridPane.setConstraints(surname, 1, 1);
+        GridPane.setConstraints(surname, 1, 2);
 
         // Label and text field for date of birth
-        Label labelDate = new Label("Date of birth:");
-        GridPane.setConstraints(labelDate, 0, 2);
+        Label labelDate = new Label("Birth Date:");
+        GridPane.setConstraints(labelDate, 0, 3);
         TextField dob = new TextField();
         dob.setPromptText("DDMMYYYY");
-        GridPane.setConstraints(dob, 1, 2);
-/*
+        GridPane.setConstraints(dob, 1, 3);
+
         // Label and text field for phone
-        Label labelPhone = new Label("Phone");
-        GridPane.setConstraints(labelDate, 0, 4);
+        Label labelPhone = new Label("Phone:");
+        GridPane.setConstraints(labelPhone, 0, 4);
         TextField phone = new TextField();
-        dob.setPromptText("phone");
-        GridPane.setConstraints(dob, 1, 4);
+        phone.setPromptText("phone");
+        GridPane.setConstraints(phone, 1, 4);
 
         // Label and text field for email
-        Label labelEmail = new Label("Email");
-        GridPane.setConstraints(labelDate, 0, 5);
+        Label labelEmail = new Label("Email:");
+        GridPane.setConstraints(labelEmail, 0, 5);
         TextField email = new TextField();
-        dob.setPromptText("email");
-        GridPane.setConstraints(dob, 1, 5);
-*/
+        email.setPromptText("email");
+        GridPane.setConstraints(email, 1, 5);
+
         // Add elements to layout and create scene
-        gridOpenAccount.getChildren().addAll(labelFirstname, firstname, labelSurname, surname, labelDate, dob);
+        gridOpenAccount.getChildren().addAll(labelTitle, labelFirstname, firstname, labelSurname, surname, labelDate, dob, labelPhone, phone, labelEmail, email);
 
-        //////////////////////////////////
-        //////// CREATE SCENEMAIN ////////
-        //////////////////////////////////
+        ////////////////////////////////////////
+        //////// CREATE SCENENEWACCOUNT ////////
+        ////////////////////////////////////////
 
-        // Create different elements
-        Label label1 = new Label("Welcome to our Bank Application.\nClick one of the available actions:");
+        // Create radio buttons to select account type
+        Label labelTitle2 = new Label("Select account type:");
+        ToggleGroup group = new ToggleGroup();
+        RadioButton rbCurrent = new RadioButton("Current");
+        rbCurrent.setUserData("Current");
+        rbCurrent.setToggleGroup(group);
+        rbCurrent.setSelected(true);
+        RadioButton rbDeposit = new RadioButton("Deposit");
+        rbDeposit.setUserData("Deposit");
+        rbDeposit.setToggleGroup(group);
+        RadioButton rbISA = new RadioButton("ISA");
+        rbISA.setUserData("ISA");
+        rbISA.setToggleGroup(group);
+
+        // Create open a new account and back to main button
+        Button buttonCancel = new Button("Cancel");
         Button buttonNewAccount = new Button("Open a New Account");
-        Button buttonProcessAccount = new Button("Process an Account");
-        Button buttonSearchAccounts = new Button("Search Current Accounts");
-        Button buttonLogout = new Button("Logout");
-        // buttonLogout.setOnAction(e -> logout());
         Label label2 = new Label("Copyright 2017, Ioannis Gkourtzounis");
 
         // Set a Column of Buttons to the Same Width
         // http://docs.oracle.com/javafx/2/layout/size_align.htm
+        buttonCancel.setMaxWidth(Double.MAX_VALUE);
         buttonNewAccount.setMaxWidth(Double.MAX_VALUE);
-        buttonProcessAccount.setMaxWidth(Double.MAX_VALUE);
-        buttonSearchAccounts.setMaxWidth(Double.MAX_VALUE);
-        buttonLogout.setMaxWidth(Double.MAX_VALUE);
 
-        // Set layout: vbox1 for Top, hbox1 for Center, hbox2 for Bottom
-        //VBox vbox1 = new VBox();
-        //vbox1.setPadding(new Insets(50, 50, 20, 50));
+        // Set layout: vbox1 for Center, vbox2 for Right, hbox1 for Bottom
+        VBox vbox1 = new VBox(10);
+        vbox1.setPadding(new Insets(20, 80, 10, 80));
         VBox vbox2 = new VBox(10);
-        vbox2.setPadding(new Insets(0, 80, 10, 0));
+        vbox2.setPadding(new Insets(44, 80, 10, 0));
         HBox hbox1 = new HBox();
-        hbox1.setPadding(new Insets(0, 0, 10, 50));
-        HBox hbox2 = new HBox();
-        hbox2.setPadding(new Insets(0, 50, 10, 50));
+        hbox1.setPadding(new Insets(0, 50, 10, 50));
 
         // Add elements to layouts
-        //vbox1.getChildren().addAll(hboxFirstname);
-        vbox2.getChildren().addAll(buttonNewAccount, buttonProcessAccount, buttonSearchAccounts, buttonLogout);
-        hbox1.getChildren().addAll(label1);
-        hbox2.getChildren().addAll(label2);
+        vbox1.getChildren().addAll(labelTitle2, rbCurrent, rbDeposit, rbISA);
+        vbox2.getChildren().addAll(buttonCancel, buttonNewAccount);
+        hbox1.getChildren().addAll(label2);
 
         // Add layouts to borderpane
         // https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/BorderPane.html
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(gridOpenAccount);
+        borderPane.setCenter(vbox1);
         borderPane.setRight(vbox2);
-        borderPane.setCenter(hbox1);
-        borderPane.setBottom(hbox2);
+        borderPane.setBottom(hbox1);
 
-        // Create "sceneMain"
-        sceneMain = new Scene(borderPane, 600, 400);
+        // Create "sceneNewAccount"
+        sceneNewAccount = new Scene(borderPane, 600, 400);
 
-        ////////////////////////////////////////
-        //////// DISPLAY SCENEENTERINFO ////////
-        ////////////////////////////////////////
+        /////////////////////////////////////////
+        //////// DISPLAY SCENENEWACCOUNT ////////
+        /////////////////////////////////////////
 
         // Display "sceneEnterInfo" when starting the application
-        window.setScene(sceneMain);
+        window.setScene(sceneNewAccount);
         window.setOnCloseRequest(e -> {
             // Prevent default close action with consume()
             e.consume();
